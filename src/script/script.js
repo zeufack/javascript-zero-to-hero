299a2e24ef4c4107efdd46404aabdc78a71a7994
zeufack <zeufackp@gmail.com>
2019-09-10 23:10:21 +0100

    try web component

--- src/script/script.js
+++ src/script/script.js
@@ -1,36 +1,28 @@
-// let element = document.createElement('p');
-// element.textContent = 'je suis le king';
-// document.body.appendChild(element);
+"use strict";
 
-// console.log([1,3,4,7].reduce( function(a, b) {
-//     return a + b;
-// }));
+class AlphormHello extends HTMLElement {
+    constructor() {
+        super();
+    }
 
-// var arr = [1, 2, 1, 5, 9, 5];
-// arr.reduce((prev, number) => {
-// if(prev.indexOf(number) === -1) {
-// prev.push(number);
-// }
-// console.log(prev)
-// return prev;
-// }, []);
-// copy an array to an other one 
-let arr = [1, 2, 1, 5, 9, 5];
-let arr2 = [...arr];
+    connectedCallback() {
+        console.log(
+            'connectedCallback'
+        )
+        let shadowDom = this.attachShadow({mode: 'open'});
+        let template = document.querySelector('#templateHelloAlphorm');
+        shadowDom.innerHTML = template.innerHTML;
+    }
+    disconnectedCallback() {
+        console.log(
+            'connectedCallback'
+        )
+    }
+    attributeChangeCallback() {
+        console.log(
+            'connectedCallback'
+        )
+    }
+}
 
-// arr.map(function (element) {
-//     arr2.push(element);
-// });
-
-// console.log(arr2);
-
-//removin duplicate element 
-
-var uniqArray = arr.filter(function(value, index, self) {
-    console.log([self],value, index);
-    return (self.indexOf(value) === index);
-    
-});
-
-console.log(`arr ${[uniqArray]}`);
-
+window.customElements.define('alphorm-hello', AlphormHello);
